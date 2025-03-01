619eae7d5be8b135912eb831daaa89c38cfbee94
Osvaldo Alves <oalves.neto@outlook.com>
2025-03-01 09:19:50 -0300

    feat: add header and footer components

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <br />
+        <h3>Developed by oalvesxp</h3>
+      </div>
+    </footer>
+  )
+}
---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1>Osvaldo A' Blog </h1>
+      </div>
+    </header>
+  )
+}
