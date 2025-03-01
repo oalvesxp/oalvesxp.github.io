fbed3239a553a4ad1fe1cdeaf1867aeb639b5bb2
Osvaldo Alves <oalves.neto@outlook.com>
2025-03-01 00:33:33 -0300

    fix: improving post page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,22 +1,20 @@
 import { getPostContent } from '@/functions/get-post-content'
 import Markdown from 'markdown-to-jsx'
 
-interface PostsPageProps {
+interface PostPageProps {
   params: Promise<{
     slug: string
   }>
 }
 
-export default async function PostPage(props: PostsPageProps) {
-  const { slug: id } = await props.params
-  const { content } = getPostContent({ slug: id })
+export default async function PostPage(props: PostPageProps) {
+  const { slug } = await props.params
+  const { content } = getPostContent({ slug })
+
   return (
     <>
       <div>
-        <h1>Post page!</h1>
-      </div>
-      <div>
-        <h2>{id}</h2>
+        <h1>{slug}</h1>
         <Markdown>{content}</Markdown>
       </div>
     </>
