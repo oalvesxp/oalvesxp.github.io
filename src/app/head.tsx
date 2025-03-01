08f2f1d47c6d80be77ad5f3754cb83f73540ec8b
Osvaldo Alves <oalves.neto@outlook.com>
2025-02-28 22:31:05 -0300

    feat: setup biome and apply lint fix

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Hi</title>
+      <meta content="width=device-widthm, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
