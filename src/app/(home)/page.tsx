3e21f1a7ce5f3ff1fb0ec83eb7e7995b107b075e
Osvaldo Alves <oalves.neto@outlook.com>
2025-03-16 18:21:47 -0300

    feat: add maintenance information

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+      <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
+        Em breve
+      </h1>
+      <p className="text-white text-lg mb-8">
+        Estamos trabalhando duro para trazer algo incrível para você. Fique ligado!
+      </p>
+    </div>
+  );
+}
