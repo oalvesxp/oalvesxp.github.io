b16161c73b5b1907318e24ab4e2e02a131def76a
Osvaldo Alves <oalves.neto@outlook.com>
2025-03-29 10:57:41 -0300

    chore: setup biome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
-import "./globals.css";
+import './globals.css'
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
